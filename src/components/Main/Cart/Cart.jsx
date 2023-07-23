fe596fcf13295615d51dcbcbeee9dc24a9a1a014
Keely <hellennnnn1@gmail.com>
2023-07-23 20:12:31 +0800

    Add Main-cart

--- src/components/Main/Cart/Cart.jsx
+++ src/components/Main/Cart/Cart.jsx
@@ -1,61 +1,59 @@
- import style from './Cart.module.css'
- import minus from '@/assets/icons/minus.svg'
- import plus from '@/assets/icons/plus.svg'
+import style from './Cart.module.css'
+import minus from '@/assets/icons/minus.svg'
+import plus from '@/assets/icons/plus.svg'
  
+const cartData = [{
+    id: '1',
+    name: '貓咪罐罐',
+    img: 'https://picsum.photos/300/300?text=1',
+    price: 100,
+    quantity: 2,
+  },{
+    id: '2',
+    name: '貓咪干干',
+    img: 'https://picsum.photos/300/300?text=2',
+    price: 200,
+    quantity: 1,
+  }]
 
- export default function Cart() {
- return(
-  <section className={style.cartContainer}>
-    <h3 className="cart-title">購物籃</h3>
-    <section className="product-list" data-total-price={0}>
-      <div
-        className={style.productContainer}
-        data-count={0}
-        data-price={3999}
-      >
-        <img className={style.imgContainer} src="https://picsum.photos/300/300?text=1" />
-        <div className={style.productInfo}>
-          <div className={style.productTitle}>
-            <div className={style.productName}>破壞補丁修身牛仔褲</div>
-            <div className={style.productPrice}>$999</div>
-          </div>
-          <div className={style.controlContainer}>
-            <div className={style.productControl}>
-              <img src={minus} alt="minus" />
-                <span className={style.productCount}>1</span>
-              <img src={plus} alt="plus" />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div
-        className={style.productContainer}
-        data-count={0}
-        data-price={1299}
-      >
-        <img className={style.imgContainer} src="https://picsum.photos/300/300?text=1" />
-        <div className={style.productInfo}>
-          <div className={style.productTitle}>
-            <div className={style.productName}>破壞補丁修身牛仔褲</div>
-            <div className={style.productPrice}>$999</div>
-          </div>
-          <div className={style.controlContainer}>
-            <div className={style.productControl}>
-              <img src={minus} alt="minus" />
-                <span className={style.productCount}>1</span>
-              <img src={plus} alt="plus" />
-            </div>
-          </div>
-        </div>
-      </div>
+
+export default function Cart() {
+
+  return (
+    <section className={style.cartContainer}>
+      <h3>購物籃</h3>
+      <section data-total-price={0}>
+            { cartData.map(data => 
+              <div
+                className={style.productContainer}
+                data-count={0}
+                data-price={3999}
+                key={data.id}
+              >
+                <img className={style.imgContainer} src={data.img} />
+                <div className={style.productInfo}>
+                  <div className={style.productTitle}>
+                    <div className={style.productName}>{data.name}</div>
+                    <div className={style.productPrice}>${data.price}</div>
+                  </div>
+                  <div className={style.controlContainer}>
+                    <div className={style.productControl}>
+                      <img src={minus} alt="minus" />
+                      <span className={style.productCount}>{data.quantity}</span>
+                      <img src={plus} alt="plus" />
+                    </div>
+                  </div>
+              </div>
+          </div> 
+        )}   
+      </section>
+      <section className={style.cartShipping}>
+        <div className={style.text}>運費</div>
+        <div className={style.price}>免費</div>
+      </section>
+      <section className={style.total}>
+        <div className={style.text}>小計</div>
+        <div className={style.price}>$1998</div>
+      </section>
     </section>
-    <section className={style.cartShipping}>
-      <div className={style.text}>運費</div>
-      <div className={style.price}>免費</div>
-    </section>
-    <section className={style.total}>
-      <div className={style.text}>小計</div>
-      <div className={style.price}>$1998</div>
-    </section>
-  </section>
 )}
