c87c7b9c7de6fa65a9bcc1be791e4fb4464dffce
Keely <hellennnnn1@gmail.com>
2023-08-01 23:45:33 +0800

    new commit

--- src/components/Main/Step/ProgressControl/ProgressControl.jsx
+++ src/components/Main/Step/ProgressControl/ProgressControl.jsx
@@ -7,7 +7,7 @@ export default function ProgressControl({next, up, step}) {
       <hr />
       <div className={styles.container}>
         <div className={styles.control}>
-        {step > 1 && (<section className={styles.back} data-phase="shipping">
+          {step > 1 && (<section className={styles.back} data-phase="shipping">
           <button onClick={up} style={{cursor: "pointer"}} className={styles.buttonTwo} >
             <img src={rightArrow} alt="rightArrow" style={{transform: 'rotate(180deg'}}/> 上一步 
           </button>
